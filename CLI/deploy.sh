82a62f708ceae7d74ccb1203e9b3fcf55f5a964e
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-11-18 17:43:02 -0800

    Deploy script checks for Docker version

--- CLI/deploy.sh
+++ CLI/deploy.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-# check if docker is installed in vm (sudo docker -v)
-# set up daemon correctly, account for azure bugs
+
+# Make sure Docker is installed
+DOCKERVERSION=$(sudo docker -v)
+VERSIONMATCH="version 1.3"
+echo $DOCKERVERSION
+
+if expr match "$DOCKERVERSION" "$VERSIONMATCH"
+  then echo "You have a good match.  Props!"
+else
+  echo "You need to update your Docker installation.  NO PROPS!"
+fi
+
+cat /etc/default/docker.io | sed 's/0.0.0.0/localhost/g' | sed 's/tlsverify/tls/' | sudo tee /etc/default/docker.io
+
+export DOCKER_OPTS="$(cat /etc/default/docker.io | grep DOCKER_OPTS | sed 's/DOCKER_OPTS=//' | sed 's/\"//g')"
+sudo service docker.io restart
+
+sudo docker $DOCKER_OPTS -d &
 
 echo "Pulling image from DockerHub"
-docker pull dockerRepoName
+sudo docker $DOCKER_OPTS pull willchen90/yo-mean:copied
 
 echo "Starting a mongo container"
-docker run --restart=always -d --name db mongo:latest
+sudo docker $DOCKER_OPTS run --restart=always -d --name db mongo:latest
 
 echo "Creating application container"
 echo "Linking to mongo container"
 echo "Running application script"
-docker run --restart=always -i -t -p 80:9000 --link db:dbLink dockerRepoName sh prod/src/app.sh
+sudo docker $DOCKER_OPTS run --restart=always -p 80:9000 --link db:dbLink willchen90/yo-mean:copied
 
-echo "Your application is deployed at: http://Cloud-service-name:80"
+# echo "Your application is deployed at: http://Cloud-service-name:80"
